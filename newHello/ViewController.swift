b8b250bf8dfb54be627264e472a4e80b1bddcb80
Vison <vison2507@gmail.com>
2020-09-30 18:50:11 +0700

    Initial Commit

---
+++ newHello/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newHello
+//
+//  Created by Vĩ Sơn on 9/30/20.
+//  Copyright © 2020 Vĩ Sơn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
