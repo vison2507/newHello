0cff24988667884cd8d414c46f5fa084aa410377
Vison <vison2507@gmail.com>
2020-09-30 19:08:08 +0700

    new branch

--- newHello/ViewController.swift
+++ newHello/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Git Tutorial")
         print("Hi there, this is branch develop")
+        print("Hello, this is commit to remote")
         // Do any additional setup after loading the view.
     }
 
